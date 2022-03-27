584b674438dc55270fc3f245baad8a5b9ef741e6
7ours <102314842+7ours@users.noreply.github.com>
2022-03-27 18:42:33 +0200

    Update Vignere_Cipher.cpp

--- Vignere_Cipher.cpp
+++ Vignere_Cipher.cpp
@@ -5,29 +5,42 @@
 using namespace std;
 
 // declartion of functions i made and used in program.
+
+// encryption function takes message and keyword then prints encrypted message.
 void encryption(string message, string keyword);
+
+// function takes any string and returns all of it in uppercase.
 string upper_letters(string word);
+
+// function checks if input is valid because message length should be less than 80 and keyword length should be less than 8.
+// if input is invalid fuction will ask user to try again.
 bool check_input(string text, string keyword);
+
+// decryption function takes message and keyword then prints decrypted message.
 void decryption(string message, string keyword);
+
+// function which asks user if he want to use encryption or decryption and returns his choice.
 int choose_mode();
+
+// function contains all funtions and the final result.
 void program();
 
 int main()
 {
-	program();
+	program(); // call the program function.
 }
 
 void encryption(string message, string keyword)
 {
 	int x = 0;
 	cout << "The encrypted message => ";
-	for (int i = 0; i < message.length(); i++)
+	for (int i = 0; i < message.length(); i++) // loop on all characters in message and add it to keyword.
 	{
-		if ( (65 <= message[i] && message[i] <= 90) )
+		if ( (65 <= message[i] && message[i] <= 90) ) // checks if character is alphabetic or not if it's alphabetic then it will be encrypted if not it will remain.
 		{
-			cout << char(((message[i] + keyword[x]) % 26) + 65);
+			cout << char(((message[i] + keyword[x]) % 26) + 65); // equation of encryption.
 			x++;
-			if (x == (keyword.length()))
+			if (x == (keyword.length())) // if it reatched the end of keyword it will starts form beginning again.
 			{
 				x = 0;
 			}
@@ -35,7 +48,7 @@ void encryption(string message, string keyword)
 		else
 		{
 			x++;
-			cout << message[i];
+			cout << message[i]; // prints the character without changes if it's alphabetic.
 		}
 	}
 }
@@ -44,7 +57,7 @@ string upper_letters(string word)
 {
 	for (int i = 0; i < word.length(); i++)
 	{
-		if ((97 <= word[i] && word[i] <= 122))
+		if ((97 <= word[i] && word[i] <= 122)) // checks the lowercase letters only and convert it ot uppercase.
 		{
 			word[i] -= 32;
 		}
@@ -118,19 +131,20 @@ void program()
 {
 	string message, keyword;
 
-	if (choose_mode() == 1)
+	if (choose_mode() == 1) // at first ask user if he wants to use encryption or decryption and checks if input is valid or not.
 	{
+		// takes the message and keyword from user.
 		cout << "Enter message you want to encrypt please: " << endl;
 		getline(cin, message);
 		cout << "Enter keyword please: " << endl;
 		getline(cin, keyword);
-		if (check_input(message, keyword))
+		if (check_input(message, keyword)) // checks if input is valid or not.
 		{
 			encryption(upper_letters(message), upper_letters(keyword));
 		}
 		else
 		{
-			cout << "Invalid input please try again." << endl;
+			cout << "Invalid input please try again." << endl; // if input is invalid prints this message and ask user to try again.
 			program();
 		}
 	}
